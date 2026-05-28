6bd1146a6ef8fd35b3d4c9fef27a0be56dce5ec7
NurvyyMelon <adinnaufal9@gmail.com>
2026-05-28 15:32:06 +0700

    Perbaiki metode display() di kelas Queues untuk menangani kasus ketika FRONT > REAR

--- queue.cpp
+++ queue.cpp
@@ -92,3 +92,11 @@ private:
             }
             cout << endl;
         }
+         else
+        {
+            // jika FRONT > REAR, iterasi dari FRONT hingga akhir array
+            while (FRONT_Position <= max - 1)
+            {
+                cout << queue_array[FRONT_Position] << "   ";
+                FRONT_Position++;
+            }
