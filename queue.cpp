350a20d1ccf030319567bf7a5f80bffb442fc371
NurvyyMelon <adinnaufal9@gmail.com>
2026-05-28 15:29:03 +0700

    Perbaiki metode insert() di kelas Queues dengan menambahkan logika untuk menangani posisi rear saat antrian kosong

--- queue.cpp
+++ queue.cpp
@@ -34,3 +34,8 @@ private:
             FRONT = 0;
             REAR = 0;
         }
+           else
+        {
+            // jika rear berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
